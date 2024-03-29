2b1ca3bfef977d7706a2ae9fda8ac34b4deae9d2
FLuiz22 <filipeluiz39@gmail.com>
2024-03-29 10:34:38 -0300

    Feat: project vue.ts started

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import ElementPlus from 'element-plus';
+import VueMask from 'vue-the-mask';
+import router from './router/index.ts';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VueMask);
+app.use(ElementPlus);
+app.use(router);
+
+app.mount('#app');
