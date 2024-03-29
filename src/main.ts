1ea73973f3f3f05be4cc2d292af95650965cd772
FLuiz22 <filipeluiz39@gmail.com>
2024-03-29 17:21:15 -0300

    Build: Element-plus icons added

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import App from './App.vue';
 import ElementPlus from 'element-plus';
 import VueMask from 'vue-the-mask';
 import router from './router/index.ts';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 const app = createApp(App);
 
 app.use(VueMask);
 app.use(ElementPlus);
 app.use(router);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.mount('#app');
