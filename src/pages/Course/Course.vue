1eb561fe214e0f8594981cd15307068af79cdfd9
FLuiz22 <filipeluiz39@gmail.com>
2024-06-05 17:26:32 -0300

    Feat: New page, header added

---
+++ src/pages/Course/Course.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home">
+        <div class="header">
+            <div class="lhead">
+                <el-image class="logo" :src="logo" />
+                <p class="htex">UF Road</p>
+            </div>
+
+            <div class="rhead">
+                <el-image class="back" :src="back" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @import './Course.scss';
+</style>
+
+<script lang="ts">
+    import scriptModule from './Course.ts';
+
+    export default {
+        name: scriptModule.name,
+        data: scriptModule.data,
+        methods: scriptModule.methods
+    }
+</script>
